9b88971d9241f3ca19034962cf5f013b87ae1009
Maria Laura Rosano <marialaurarosano@gmail.com>
2023-04-14 17:10:35 -0300

    Migrating to AS4 course

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
+
+const port = process.env.PORT || 4000;
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
   });
 
-  const { url, port } = await server.listen({port: process.env.PORT || 4000});
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+    listen: { port },
+  });
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
